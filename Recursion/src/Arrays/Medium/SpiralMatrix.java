e462d45f6991e692d05a10efa6e7615021ef77b4
jaganyv <jaganyv1@gmail.com>
2026-03-17 17:53:58 +0530

    RotateImage And SpiralMatrix Completed

---
+++ Recursion/src/Arrays/Medium/SpiralMatrix.java
@@ -0,0 +1,66 @@
+package Arrays.Medium;
+
+import java.util.*;
+
+public class SpiralMatrix {
+
+    static class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+
+            List<Integer> result = new ArrayList<>();
+
+            int top = 0;
+            int bottom = matrix.length - 1;
+            int left = 0;
+            int right = matrix[0].length - 1;
+
+            while (top <= bottom && left <= right) {
+
+                // top row
+                for (int i = left; i <= right; i++) {
+                    result.add(matrix[top][i]);
+                }
+                top++;
+
+                // right column
+                for (int i = top; i <= bottom; i++) {
+                    result.add(matrix[i][right]);
+                }
+                right--;
+
+                // bottom row
+                if (top <= bottom) {
+                    for (int i = right; i >= left; i--) {
+                        result.add(matrix[bottom][i]);
+                    }
+                    bottom--;
+                }
+
+                // left column
+                if (left <= right) {
+                    for (int i = bottom; i >= top; i--) {
+                        result.add(matrix[i][left]);
+                    }
+                    left++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        List<Integer> ans = obj.spiralOrder(matrix);
+        System.out.println(ans);
+    }
+}
