61824567de8b012786bcb9ace11e1b6663245b31
jaganyv <jaganyv1@gmail.com>
2026-03-23 14:53:10 +0530

    Done An Simple Binary Search Pattern

---
+++ Recursion/src/Own_Stuffs/RotatedBinarySearch.java
@@ -0,0 +1,33 @@
+package Own_Stuffs;
+
+public class RotatedBinarySearch {
+    static int rotatedBinarySearch(int[] arr,int target){
+        int low = 0 , high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high-low) / 2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && target < arr[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid]<=target && target < arr[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int answer =  rotatedBinarySearch(arr,target);
+        System.out.println(answer);
+    }
+}
